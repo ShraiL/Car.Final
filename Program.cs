aed9bd6aa0534200f6a71378e510905ef8a60ac7
ShraiL <lashraieri@gmail.com>
2025-05-31 15:01:07 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace LashaMurgvaLominadzeShraieri.Final
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
